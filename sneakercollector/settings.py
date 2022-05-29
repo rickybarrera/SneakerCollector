d9da7f58acb46ed3b34ae34222bca0d3e4ca5b4f
ricky barrera <rickybarrrera0@gmail.com>
2022-05-28 19:07:06 -0700

    deploys

--- sneakercollector/settings.py
+++ sneakercollector/settings.py
@@ -128,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/sneakers/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
